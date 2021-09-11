cb6d81f07a4758e37f9316c0cb81a31d0080eee2
DESKTOP-HGJTF3M\maxim <maximilian.zmek@gmail.com>
2021-09-11 17:19:27 +0200

    - config changes for release - added starter items

--- kubejs/data/anima/functions/starter_items.mcfunction
+++ kubejs/data/anima/functions/starter_items.mcfunction
@@ -1,1 +1,5 @@
-give @s ftbquests:book
+give @s inmis:baby_backpack
+give @s mcdw:staff_battlestaff
+give @s antiqueatlas:empty_antique_atlas
+give @s minecraft:spyglass
+give @s dehydration:leather_flask
--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -1,20 +1,52 @@
 // priority: 0
 
-settings.logAddedRecipes = true
-settings.logRemovedRecipes = true
-settings.logSkippedRecipes = false
-settings.logErroringRecipes = true
-
 console.info('Hello, World! (You will see this line every time server resources reload)')
 
-onEvent('recipes', event => {
-	// Change recipes here
+events.listen('recipes', event => {
+  // Change recipes here
+  event.remove({id: 'minecraft:tnt'})
+  event.shapeless(item.of('minecraft:slime_ball', 1), ['minecraft:milk_bucket', 'minecraft:sugar', 'minecraft:wheat'])
+  event.shapeless(item.of('minecraft:blaze_rod', 1), ['minecraft:gold_ingot', 'minecraft:stick', 'minecraft:nether_brick'])
+  event.shapeless(item.of('minecraft:leather', 1), ['minecraft:rotten_flesh', 'minecraft:rotten_flesh', 'minecraft:rotten_flesh', 'minecraft:rotten_flesh', 'minecraft:rotten_flesh'])
+  event.shapeless(item.of('minecraft:cobweb', 1), ['minecraft:string', 'minecraft:string', 'minecraft:string', 'minecraft:string', 'minecraft:string'])
+
+  event.shapeless(item.of('porkchop', 1), ['minecraft:rotten_flesh', 'minecraft:sugar'])
+  event.shapeless(item.of('chicken', 1), ['minecraft:rotten_flesh', 'minecraft:melon_seeds'])
+  event.shapeless(item.of('beef', 1), ['minecraft:rotten_flesh', 'minecraft:wheat_seeds'])
+  event.shapeless(item.of('minecraft:phantom_membrane', 3), ['minecraft:rotten_flesh', 'minecraft:slime_ball', 'minecraft:leather'])
+  event.shapeless(item.of('minecraft:egg', 1), ['minecraft:rotten_flesh', 'minecraft:slime_ball', 'minecraft:bone_meal'])
+
+
+  event.shaped(item.of('minecraft:ghast_tear', 1), [
+    'AAA',
+    'ABA',
+    'AAA'
+    ], {
+    A: 'minecraft:phantom_membrane',
+    B: 'minecraft:diamond'
+
+  })
+
+
+  event.shaped(item.of('origins:orb_of_origin', 1), [
+    'ABC',
+    'DEF',
+    'GHI'
+    ], {
+    A: 'mcdw:item_bee_stinger',
+    B: 'minecraft:golden_carrot',
+    C: 'minecraft:cod_bucket',
+    D: 'minecraft:seagrass',
+    E: 'minecraft:diamond',
+    F: 'minecraft:ender_eye',
+    G: 'minecraft:feather',
+    H: 'blockus:rotten_flesh_block',
+    I: 'minecraft:slime_ball'
+  })
+
+
 })
 
-onEvent('item.tags', event => {
-	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
-	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
-
-	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
-	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+events.listen('item.tags', event => {
+  // Change item tags here
 })
